8d44a9588c5a585288341d101335268edd818797
ibrah3m <ibrahem.developer@gmail.com>
2022-04-08 02:56:42 +0300

    Update popup.js
    finally , now its getting only not started and paid sorted according to minimum duration

--- popup.js
+++ popup.js
@@ -1,78 +1,85 @@
-$(function(){
+$(function () {
 
-    jQuery.ajax({
-        url: 'https://www.udemy.com/api-2.0/users/me/subscribed-courses/?page_size=100',
-        success: function (data) {
-    
-            enitre_courses = [];
-            enitre_courses["results"] = data.results.filter(results => results.is_paid = true)
-            next = data.next;
-    
-    
-            while (next != null) {
-    
-                if (next != null) {
-    
-    
-                    jQuery.ajax({
-                        url: data.next,
-                        success: function (next_data) {
-                            enitre_courses.results.push(next_data.results.filter(next_results => next_results.is_paid = true));
-                            next = next_data.next;
-                        },
-                        async: false
-                    });
-    
-    
-    
-    
+    fetch('https://www.udemy.com/api-2.0/users/me/subscribed-courses/?page_size=100&max_progress=0&min_progress=0').then(r => r.json()).then(
+        next_data => {
+            entire_courses = [];
+
+            entire_courses["results"] = next_data.results.filter(results => results.is_paid = true),
+                next = next_data.next;
+
+
+        }
+    ).then(async r => {
+
+        while (next != null) {
+
+            await fetch(next).then(r => r.json()).then(
+                next_data => {
+                    entire_courses.results.push(next_data.results.filter(results => results.is_paid = true)),
+                        next = next_data.next
+
+
                 }
-    
-            }
-    
-    
-    
-        },
-        async: false
-    });
-    // get each course details
-    
-    $.each(enitre_courses.results, function (key, value) {
-        jQuery.ajax({
-            url:'https://www.udemy.com/api-2.0/course-landing-components/'+value.id+'/me?components=incentives',
-            success: function (next_data) {
-                value.length=juration.parse(next_data.incentives.video_content_length)
-    
-                
-            },
-            async: false
-        });
-    
+            )
+
+        }
+
+
+                entire_mapped_courses = Promise.all(entire_courses.results.map(async function (value) {
+
+                    await fetch('https://www.udemy.com/api-2.0/course-landing-components/' + value.id + '/me?components=incentives').then(r => r.json()).then(
+                        next_data => {
+                            try {
+                                value.video_length = juration.parse(next_data.incentives.video_content_length)
+                            } catch (err) {
+                                console.log(err)
+                            }
+
+                        }
+                    ).catch((error) => {
+                        console.log(error)
+                    });
+
+                })).then(results => {
+
+
+
+                    entire_courses.results.sort(function (a, b) {
+
+                        return a.video_length - b.video_length;
+                    });
+
+
+                    // view the list 
+
+                    $.each(entire_courses.results, function (key, value) {
+                        $('#list').append(
+
+                            `
+                        <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="` + value.image_240x135 + `" alt="Card image cap">
+                                <div class="card-body">
+                                <h5 class="card-title">` + value.title + `</h5>
+                                <a href="https://www.udemy.com` + value.url + `"  target="_blank" class="btn btn-primary">Go</a>
+                                </div>
+                        </div>
+                        `
+
+
+                        );
+
+                    })
+
+
+                })
+
+
+
+
+
     })
-    // sort desc
-    enitre_courses.results.sort(function(a, b) {
-        return a.length - b.length;
-    });
 
 
-    // view the list 
-      
-    $.each(enitre_courses.results, function (key, value) {
-        $('#list').append(
 
-            `
-            <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="`+value.image_240x135+`" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">`+value.title+`</h5>
-                    <a href="https://www.udemy.com`+value.url+`" class="btn btn-primary">Go</a>
-                    </div>
-            </div>
-            `
-        
-        
-            );
-    
-    })
-   
-  });
+
+});
